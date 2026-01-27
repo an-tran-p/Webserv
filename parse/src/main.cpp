370091c224cd15814345ffea8865baf56394a739
Juhyeon Lee <juhyeonl@c1r3p6.hive.fi>
2026-01-27 16:48:28 +0200

    alpha version parse

---
+++ parse/src/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhyeonl <juhyeonl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 22:07:11 by juhyeonl          #+#    #+#             */
+/*   Updated: 2026/01/27 16:45:29 by juhyeonl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "request.hpp"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	Request req;
+
+	std::vector<std::string> chunks;
+	chunks.push_back("GET /index.html HT");
+	chunks.push_back("TP/1.1\r\nHost: localhost\r\nContent-Length: 5\r\n\r\n");
+	chunks.push_back("hello");
+
+	std::cout << "--- Parsing Start ---" << std::endl;
+
+	for (size_t i = 0; i < chunks.size(); ++i)
+	{
+		std::cout << "[Chunk " << i << " delivering...]" << std::endl;
+		req.parse(chunks[i]);
+
+		if (req.isDone())
+		{
+			std::cout << ">> parse done!" << std::endl;
+			break;
+		}
+		else
+			std::cout << ">> It's need more data (now state: " << i << ")" << std::endl;
+	}
+	if (req.isDone())
+	{
+		std::cout << "\n--- result ---" << std::endl;
+		std::cout << "Method: " << req.method << std::endl;
+		std::cout << "Path: " << req.path << std::endl;
+		std::cout << "Host: " << req.headers["Host"] << std::endl;
+		std::cout << "Body: " << req.body << std::endl;
+	}
+	return 0;
+}
---
+++ parse/src/parse.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhyeonl <juhyeonl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 00:16:44 by juhyeonl          #+#    #+#             */
+/*   Updated: 2026/01/27 16:45:50 by juhyeonl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "request.hpp"
+
+Request::Request() : _state(REQUEST_LINE), _content_length(0)
+{
+}
+
+Request::~Request()
+{
+}
+
+bool	Request::isDone() const
+{
+	return (_state == DONE);
+}
+
+void Request::parse(std::string chunk)
+{
+    _buffer += chunk;
+    while (_state != DONE && _state != ERROR)
+    {
+        if (_state == REQUEST_LINE)
+        {
+            size_t pos = _buffer.find("\r\n");
+            if (pos == std::string::npos)
+				return;
+            std::string line = _buffer.substr(0, pos);
+            std::stringstream ss(line);
+            ss >> method >> path >> protocol;
+            _buffer.erase(0, pos + 2);
+            _state = HEADERS;
+        }
+        else if (_state == HEADERS)
+        {
+            size_t pos = _buffer.find("\r\n");
+            if (pos == std::string::npos)
+				return ;
+            std::string line = _buffer.substr(0, pos);
+            if (line.empty())
+            {
+                _buffer.erase(0, pos + 2);
+                if (headers.count("Content-Length"))
+                {
+                    _content_length = std::atoi(headers["Content-Length"].c_str());
+                    _state = BODY;
+                }
+                else
+                    _state = DONE;
+            }
+            else
+            {
+                size_t colon = line.find(":");
+                if (colon != std::string::npos)
+                {
+                    std::string key = line.substr(0, colon);
+                    std::string value = line.substr(colon + 1);
+                    headers[key] = value;
+                }
+                _buffer.erase(0, pos + 2);
+            }
+        }
+        else if (_state == BODY)
+        {
+            if (_buffer.length() >= _content_length)
+            {
+                body = _buffer.substr(0, _content_length);
+                _buffer.erase(0, _content_length);
+                _state = DONE;
+            }
+            else
+                return ;
+        }
+    }
+}
